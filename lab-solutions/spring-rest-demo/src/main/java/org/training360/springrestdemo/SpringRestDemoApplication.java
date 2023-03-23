b4acdaaa47648702717d9c6cc8691cf982ffad2c
bkris21 <barczaykristof1993@gmail.com>
2023-03-23 14:25:43 +0100

    spring rest demo app update

--- lab-solutions/spring-rest-demo/src/main/java/org/training360/springrestdemo/SpringRestDemoApplication.java
+++ lab-solutions/spring-rest-demo/src/main/java/org/training360/springrestdemo/SpringRestDemoApplication.java
@@ -2,6 +2,7 @@ package org.training360.springrestdemo;
 
 import org.modelmapper.Condition;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeMap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
